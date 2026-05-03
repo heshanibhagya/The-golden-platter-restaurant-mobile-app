e22fb1e2f19643fbe90e89c31d98638015a27ae4
IT24101135 <IT24101135@my.sliit.lk>
2026-05-03 18:53:59 +0530

    Update backend URL to hosted link

--- mobile-app/src/config.js
+++ mobile-app/src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://192.168.1.7:5000/api';
+export const API_BASE_URL = 'https://the-golden-platter-restaurant-mobile-app.onrender.com/api';
 export const SERVER_URL = 'http://192.168.1.7:5000';
 
